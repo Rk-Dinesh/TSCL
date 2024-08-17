2a036c2ab81f3f71aa6aa16dd246fbab9b67366d
Rk-Dinesh <rdkumar881@gmail.com>
2024-08-17 11:56:20 +0530

    view form responsiveness

--- TSCL/src/Pages/request/ViewRequest.jsx
+++ TSCL/src/Pages/request/ViewRequest.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { API } from "../../Host";
 import { useLocation } from "react-router-dom";
 
-
 const ViewRequest = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -35,12 +34,12 @@ const ViewRequest = () => {
 
   return (
     <div className="h-screen overflow-y-auto no-scrollbar">
-      <div className="mx-6 my-5 font-lexend">
+      <div className="md:mx-6 mx-2  my-5 font-lexend">
         <p>Reply Complaint {data.grievance_id}</p>
         <div className="bg-white mt-2 pb-3">
-          <p className="px-5 py-3 text-lg">Request By :</p>
-          <div className="grid grid-cols-12 gap-3 mx-3 my-3">
-            <div className="col-span-3 px-5 pb-3">
+          <p className="px-5 py-2 text-lg">Request By :</p>
+          <div className="md:grid md:grid-cols-12 flex gap-3 mx-3 my-1">
+            <div className="md:col-span-4 px-5 pb-3">
               <p>{data.public_user_name}</p>
               <p>+91 {data.phone}</p>{" "}
             </div>
@@ -73,71 +72,90 @@ const ViewRequest = () => {
           </div>
           <hr />
           <div className="grid grid-cols-12 gap-2 mx-3 my-4">
-            <div className="col-span-6 border px-2 py-3 rounded">
+            <div className="md:col-span-6 col-span-12 border px-2 py-3 rounded">
               <p className="pt-2 text-lg">Grievance Details</p>
               <hr className="my-3" />
               <div className="flex flex-col gap-3 mx-2 text-base">
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Origin :</p>
-                  <p className="col-span-2">{data.grievance_mode}</p>
+                  <p className="col-span-2">Origin </p>
+                  <p className="col-span-2">: {data.grievance_mode}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Complaint Type :</p>
-                  <p className="col-span-2">{data.complaint_type_title}</p>
+                  <p className="col-span-2">Complaint Type </p>
+                  <p className="col-span-2">: {data.complaint_type_title}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Department :</p>
-                  <p className="col-span-2">{data.dept_name}</p>
+                  <p className="col-span-2">Department </p>
+                  <p className="col-span-2">: {data.dept_name}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Zone :</p>
-                  <p className="col-span-2">{data.zone_name}</p>
+                  <p className="col-span-2">Zone </p>
+                  <p className="col-span-2">: {data.zone_name}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Ward :</p>
-                  <p className="col-span-2">{data.ward_name}</p>
+                  <p className="col-span-2">Ward </p>
+                  <p className="col-span-2">: {data.ward_name}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Street :</p>
-                  <p className="col-span-2">{data.street_name}</p>
+                  <p className="col-span-2">Street </p>
+                  <p className="col-span-2">: {data.street_name}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Pincode :</p>
-                  <p className="col-span-2">{data.pincode}</p>
+                  <p className="col-span-2">Pincode </p>
+                  <p className="col-span-2">: {data.pincode}</p>
                 </div>
                 <div className="grid grid-cols-4">
-                  <p className="col-span-2">Description :</p>
-                  <p className="col-span-2">{data.complaint_details}</p>
+                  <p className="col-span-2">Description: </p>
+                  <p className="col-start-1 col-span-4 mt-2">
+                    {data.complaint_details}
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="col-span-6 border px-2 py-3 rounded">
-              <p className="pt-2 text-lg">Similar Request</p>
-              <hr className="my-3" />
-              <table className="w-full bg-gray-200 rounded">
-                <thead>
-                  <tr>
-                    <th className="items-center py-2 font-lexend whitespace-nowrap">
-                      Date/Time
-                    </th>
-                    <th className="items-center py-2 font-lexend whitespace-nowrap">
-                      Complaint No
-                    </th>
-                    <th className="items-center py-2 font-lexend whitespace-nowrap">
-                      Status
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-300">
-                  <tr>
-                    <td className="text-center py-2">15-05-2024 / 12:00 AM</td>
-                    <td className="text-center py-2">R-0001122</td>
-                    <td className="text-center py-2 text-green-600">
-                      In Progress
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="md:col-span-6 col-span-12 border px-2 py-3 rounded ">
+              <p className="pt-2 text-lg ">Similar Request</p>
+              <hr className="my-3 w-full" />
+              <div className="overflow-auto no-scrollbar">
+                <table className="w-full bg-gray-200 rounded ">
+                  <thead>
+                    <tr>
+                      <th className="items-center mx-3 my-2 font-lexend whitespace-nowrap">
+                        Date/Time
+                      </th>
+                      <th className="items-center mx-3 my-2 font-lexend whitespace-nowrap">
+                        Complaint No
+                      </th>
+                      <th className="items-center mx-3 my-2 font-lexend whitespace-nowrap">
+                        Status
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-300">
+                    <tr className="border-b-2 border-gray-300">
+                      <td className="text-center mx-3 my-2 whitespace-nowrap">
+                        15-05-2024 / 12:00 AM
+                      </td>
+                      <td className="text-center  mx-3 my-2 whitespace-nowrap">
+                        R-0001122
+                      </td>
+                      <td className="text-center  mx-3 my-2 text-green-600 whitespace-nowrap">
+                        In Progress
+                      </td>
+                    </tr>
+                    <tr className="border-b-2 border-gray-300">
+                      <td className="text-center mx-2 my-2 whitespace-nowrap">
+                        15-05-2024 / 12:00 AM
+                      </td>
+                      <td className="text-center  mx-2 my-2 whitespace-nowrap">
+                        R-0001122
+                      </td>
+                      <td className="text-center  mx-2 my-2 text-green-600 whitespace-nowrap">
+                        In Progress
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
           <div className="mx-3 my-3">
@@ -151,34 +169,62 @@ const ViewRequest = () => {
                   {new Date(data.createdAt).toLocaleDateString()}
                 </p>
                 <div className="grid grid-cols-3 divide-x-2 divide-black">
-                <p>{new Date(data.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+                  <p>
+                    {new Date(data.createdAt).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                  </p>
                   <p className="pl-5 col-span-2">Logged In</p>
                 </div>
                 <br />
                 <div className="grid grid-cols-3 divide-x-2 divide-black">
-                <p>{new Date(data.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
-                  <p className="pl-5 col-span-2">Ticket Raised {data.grievance_id}</p>
+                  <p>
+                    {new Date(data.createdAt).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                  </p>
+                  <p className="pl-5 col-span-2">
+                    Ticket Raised {data.grievance_id}
+                  </p>
                 </div>
                 <br />
                 <div className="grid grid-cols-3 divide-x-2 divide-black">
-                <p>{new Date(data.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
-                  <p className="pl-5 col-span-2">Assigned To Particular Department</p>
+                  <p>
+                    {new Date(data.createdAt).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                  </p>
+                  <p className="pl-5 col-span-2">
+                    Assigned To Particular Department
+                  </p>
                 </div>
                 <br />
                 <p className="mb-2">
                   {new Date(data.updatedAt).toLocaleDateString()}
                 </p>
                 <div className="grid grid-cols-3 divide-x-2 divide-black">
-                <p>{new Date(data.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</p>
+                  <p>
+                    {new Date(data.createdAt).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                  </p>
                   <div className="col-span-2">
                     <p className="pl-5">Status:</p>
                     <p className="pl-5 text-gray-500">{data.status}/</p>
                   </div>
                 </div>
                 <hr className="my-3" />
-                <div className="grid grid-cols-3 border-2 mx-20">
-                  <p className="text-center py-1.5">Status</p>
-                  <p className="text-center bg-gray-800 col-span-2 text-white py-1.5">
+                <div className="md:grid md:grid-cols-3 flex border-2 md:mx-20">
+                  <p className="text-center px-3 py-1.5">Status</p>
+                  <p className="text-center w-full bg-gray-800 md:col-span-2 text-white py-1.5">
                     {data.status}
                   </p>
                 </div>
