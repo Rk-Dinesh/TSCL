e1055f88bdce008167810f00fd415108bbd202af
Rk-Dinesh <rdkumar881@gmail.com>
2024-09-09 16:26:38 +0530

    APICLIENT

--- TSCL/src/ApiEndpoints/api/ApiClient.js
+++ TSCL/src/ApiEndpoints/api/ApiClient.js
@@ -7,10 +7,16 @@ const authHeaders = {
 };
 
 const API_ENDPOINTS = {
+
+  //ORGANIZATION:
+  POST_ORGANIZATION:{url:`${BASE_URL}/organization/post`,headers: authHeaders},
   GET_ORGANIZATION: {url: `${BASE_URL}/organization/get`,headers: authHeaders},
   GET_ORGANIZATIONACTIVE: {url: `${BASE_URL}/organization/getactive`,headers: authHeaders},
   DELETE_ORGANIZATION:(deleteId) => ({url: `${BASE_URL}/organization/delete?org_id=${deleteId}`,headers: authHeaders}),
-  CSV_ORGANIZATION:{url:`${BASE_URL}organization/uploadcsv`,headers: authHeaders},
+  CSV_ORGANIZATION:{url:`${BASE_URL}/organization/uploadcsv`,headers: authHeaders},
+
+  //DEPARTMENT:
+  
 };
 
 export default API_ENDPOINTS;
